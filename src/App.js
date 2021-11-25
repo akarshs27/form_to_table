2b557cd6fd5e2d9243f1d6c43079fec4c8b25a64
Akarsh Srivastava <akarshsrivastava@Akarshs-MacBook-Air.local>
2021-11-25 20:36:02 +0530

    form data to table

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
-import Player from "./components/home/Player/Player";
-import Song from "./components/home/Song/Song";
-import { useState, useRef } from "react";
-import data from "./data/data";
-import { playAudio } from "./data/util";
+import { useState } from "react";
+import EmployeeForm from "./components/EmployeeForm";
+import Table from "./components/Table";
 
 function App() {
-  const audioRef = useRef(null);
-
-  const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[0]);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  const [songInfo, setSongInfo] = useState({
-    currentTime: 0,
-    duration: 0,
-    animationPercentage: 0,
+  const [employeeDetails, setEmployeeDetails] = useState([]);
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    age: "",
+    city: "",
   });
 
-  const UpdateTimeHandler = (e) => {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    const roundedCurrent = Math.round(current);
-    const roundedDuration = Math.round(duration);
-    const percentage = Math.round((roundedCurrent / roundedDuration) * 100);
-    setSongInfo({
-      ...songInfo,
-      currentTime: current,
-      duration,
-      animationPercentage: percentage,
-    });
-  };
-
-  const songEndHandler = async () => {
-    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
-    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
-    playAudio(isPlaying, audioRef);
-    return;
-  };
-
   return (
-    <div className="background h-screen w-full">
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col relative">
-          <div className="song">
-            <Song currentSong={currentSong} />
-          </div>
-          <div className="player">
-            <Player
-              isPlaying={isPlaying}
-              setIsPlaying={setIsPlaying}
-              currentSong={currentSong}
-              audioRef={audioRef}
-              songInfo={songInfo}
-              setSongInfo={setSongInfo}
-              songs={songs}
-              setCurrentSong={setCurrentSong}
-              setSongs={setSongs}
-            />
-          </div>
-          <audio
-            onLoadedMetadata={UpdateTimeHandler}
-            onTimeUpdate={UpdateTimeHandler}
-            ref={audioRef}
-            src={currentSong.audio}
-            onEnded={songEndHandler}
-          ></audio>
-        </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-10 mx-auto">
+        <EmployeeForm
+          formData={formData}
+          setFormData={setFormData}
+          employeeDetails={employeeDetails}
+          setEmployeeDetails={setEmployeeDetails}
+        />
+        {employeeDetails.length > 0 ? (
+          <>
+            <Table employeeDetails={employeeDetails} />
+          </>
+        ) : (
+          ""
+        )}
       </div>
-    </div>
+    </section>
   );
 }
 
